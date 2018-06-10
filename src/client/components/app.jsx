b09bccf88470c3e6b64b6dad959c6e0e57da2aca
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-06-09 19:27:31 -0700

    react set up initial api call

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,18 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      location: 'San Francisco',
+      description: '',
+      temperature: ''
     };
   }
+
+  componentWillMount() {
+    axios.get('/forecast')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          location: response.data.city.name,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
 
   render() {
     return (
       <div>
-        <h1> whyyy </h1>
+        <h1>{this.state.location}</h1>
+        <div className="days-of-week">
+          <div className="day">
+            <p>Monday</p>
+            <img src="https://media.giphy.com/media/3ohhwGUroCf7Ka7bc4/giphy.gif" alt="sunny" />
+            <p>{this.state.description}</p>
+            <p>{this.state.temperature}</p>
+          </div>
+          <div className="day">
+            <p>Tuesday</p>
+            <img src="https://media.giphy.com/media/3ohhwGUroCf7Ka7bc4/giphy.gif" alt="sunny" />
+            <p>Sunny!</p>
+          </div>
+          <div className="day">
+            <p>Wednesday</p>
+            <img src="https://media.giphy.com/media/3ohhwGUroCf7Ka7bc4/giphy.gif" alt="sunny" />
+            <p>Sunny!</p>
+          </div>
+          <div className="day">
+            <p>Thursday</p>
+            <img src="https://media.giphy.com/media/3ohhwGUroCf7Ka7bc4/giphy.gif" alt="sunny" />
+            <p>Sunny!</p>
+          </div>
+          <div className="day">
+            <p>Friday</p>
+            <img src="https://media.giphy.com/media/3ohhwGUroCf7Ka7bc4/giphy.gif" alt="sunny" />
+            <p>Sunny!</p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config()
+
+// const { API_KEY } = process.env;
 
 const app = express();
 
@@ -9,5 +13,16 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '/../client/')));
+
+app.get('/forecast', (req, res) => {
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=5391997&APPID=${API_KEY}`)
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.listen(1337, () => console.log('Yo your server is running on 1337'));
